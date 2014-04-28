9ddef501fdc4583c39123f830c1d9203651431e0
Justin Smith <noisesmith@gmail.com>
2014-04-28 10:38:32 -0700

    forked from the upstream library

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject clj-rss "0.1.5"
+(defproject noisesmith/clj-rss "0.1.7"
   :description "A library for generating RSS feeds from Clojure."
-  :url "https://github.com/yogthos/clj-rss"
+  :url "https://github.com/noisesmith/clj-rss"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]])
